6b87a5764a5d7310e3813fe93db52fb66e7b891a
milagrosparianaza <mparicanazac@unsa.edu.pe>
2025-11-02 21:33:22 -0500

    Modificación de dependencias
    Se agrego la dependencia con la que se trabajara para los avatares del listado

--- Ejercicio_Practico/app/build.gradle.kts
+++ Ejercicio_Practico/app/build.gradle.kts
@@ -49,11 +49,10 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
+
+    // Para mostrar imágenes (avatars)
+    implementation("io.coil-kt:coil-compose:2.6.0")
+
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
