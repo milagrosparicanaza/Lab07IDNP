9c50daae4399a1858a72104bb0110861b7770ed8
milagrosparianaza <mparicanazac@unsa.edu.pe>
2025-11-23 23:00:49 -0500

    Mostrar resultados
    Para mostrar los resultados registrados

--- Ejercicio_Practico/app/src/main/java/com/example/listapacientesanemia/ui/ListaResultadosScreen.kt
+++ Ejercicio_Practico/app/src/main/java/com/example/listapacientesanemia/ui/ListaResultadosScreen.kt
@@ -1,4 +1,36 @@
 package com.example.listapacientesanemia.ui
 
-class ListaResultadosScreen {
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.listapacientesanemia.model.AnemiaRepository
+
+@Composable
+fun ListaResultadosScreen(navController: NavController, repo: AnemiaRepository) {
+
+    val resultados by repo.resultados.collectAsState(initial = emptyList())
+
+    Column(modifier = Modifier.padding(20.dp)) {
+        Text("Resultados de Anemia", style = MaterialTheme.typography.titleLarge)
+
+        Spacer(Modifier.height(20.dp))
+
+        LazyColumn(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+            items(resultados) { r ->
+                Card(modifier = Modifier.fillMaxWidth()) {
+                    Column(modifier = Modifier.padding(16.dp)) {
+                        Text("Paciente: ${r.paciente}")
+                        Text("Hemoglobina: ${r.hemoglobina}")
+                        Text("Diagnóstico: ${r.resultado}")
+                        Text("Fecha: ${r.fecha}")
+                    }
+                }
+            }
+        }
+    }
 }
